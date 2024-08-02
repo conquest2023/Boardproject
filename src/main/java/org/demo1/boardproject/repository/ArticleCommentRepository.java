96109acf0476a94c01bb3ca3ce686efbeac9a306
wngud1 <dlqns928@naver.com>
2024-08-02 21:02:23 +0900

    #1 -data rest설정

--- src/main/java/org/demo1/boardproject/repository/ArticleCommentRepository.java
+++ src/main/java/org/demo1/boardproject/repository/ArticleCommentRepository.java
@@ -3,6 +3,9 @@ package org.demo1.boardproject.repository;
 import org.demo1.boardproject.domain.Article;
 import org.demo1.boardproject.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long> {
 }
--- src/main/java/org/demo1/boardproject/repository/ArticleRepository.java
+++ src/main/java/org/demo1/boardproject/repository/ArticleRepository.java
@@ -2,6 +2,9 @@ package org.demo1.boardproject.repository;
 
 import org.demo1.boardproject.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article,Long> {
 }
